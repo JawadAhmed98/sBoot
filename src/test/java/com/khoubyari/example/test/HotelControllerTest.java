83c0eba32df54e938e53102f235439c5cd9aeb25
JawadAhmed98 <157863985+JawadAhmed98@users.noreply.github.com>
2024-03-06 11:48:29 -0500

    Update HotelControllerTest.java

--- src/test/java/com/khoubyari/example/test/HotelControllerTest.java
+++ src/test/java/com/khoubyari/example/test/HotelControllerTest.java
@@ -1,18 +1,14 @@
 package com.khoubyari.example.test;
 
-/**
- * Uses JsonPath: http://goo.gl/nwXpb, Hamcrest and MockMVC
- */
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khoubyari.example.Application;
 import com.khoubyari.example.api.rest.HotelController;
 import com.khoubyari.example.domain.Hotel;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +17,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -33,6 +28,7 @@ import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -42,10 +38,10 @@ public class HotelControllerTest {
     private static final String RESOURCE_LOCATION_PATTERN = "http://localhost/example/v1/hotels/[0-9]+";
 
     @InjectMocks
-    HotelController controller;
+    private HotelController controller;
 
     @Autowired
-    WebApplicationContext context;
+    private WebApplicationContext context;
 
     private MockMvc mvc;
 
@@ -55,7 +51,7 @@ public class HotelControllerTest {
         mvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
-    //@Test
+    @Test
     public void shouldHaveEmptyDB() throws Exception {
         mvc.perform(get("/example/v1/hotels")
                 .accept(MediaType.APPLICATION_JSON))
@@ -96,14 +92,6 @@ public class HotelControllerTest {
         mvc.perform(get("/example/v1/hotels/" + id)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
-
-        //todo: you can test the 404 error body too.
-
-/*
-JSONAssert.assertEquals(
-  "{foo: 'bar', baz: 'qux'}",
-  JSONObject.fromObject("{foo: 'bar', baz: 'xyzzy'}"));
- */
     }
 
     @Test
@@ -147,16 +135,10 @@ JSONAssert.assertEquals(
                 .andExpect(status().isNoContent());
     }
 
-
-    /*
-    ******************************
-     */
-
     private long getResourceIdFromUrl(String locationUrl) {
         String[] parts = locationUrl.split("/");
         return Long.valueOf(parts[parts.length - 1]);
     }
-
 
     private Hotel mockHotel(String prefix) {
         Hotel r = new Hotel();
@@ -172,14 +154,10 @@ JSONAssert.assertEquals(
         return map.writeValueAsString(r).getBytes();
     }
 
-    // match redirect header URL (aka Location header)
     private static ResultMatcher redirectedUrlPattern(final String expectedUrlPattern) {
-        return new ResultMatcher() {
-            public void match(MvcResult result) {
-                Pattern pattern = Pattern.compile("\\A" + expectedUrlPattern + "\\z");
-                assertTrue(pattern.matcher(result.getResponse().getRedirectedUrl()).find());
-            }
+        return result -> {
+            Pattern pattern = Pattern.compile("\\A" + expectedUrlPattern + "\\z");
+            assertTrue(pattern.matcher(result.getResponse().getRedirectedUrl()).find());
         };
     }
-
 }
